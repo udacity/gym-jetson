e4f6b505de7ad02511d5bdd457003e8d471127f1
alexisbcook <alexis.cook@gmail.com>
2018-03-13 14:46:07 -0600

    saving notebook

--- jetson_env.py
+++ jetson_env.py
@@ -1,19 +1,24 @@
+import math
 import numpy as np
 
 class JetsonEnv():
 
-    def __init__(self, nLED=4):
+    def __init__(self, nLED=4, max_move=15):
         self.nLED = nLED
-        self.nA = 3 # actions: (0) move left, (1) stay & push, (2) move right
+        self.nS = int(math.pow(nLED, 2))  # number of states
+        self.nA = 3                       # number of actions
+        self.max_move = max_move          # max number of moves in game
 
     def reset(self):
         self.board = np.random.choice(self.nLED)
         self.pos = np.random.choice(self.nLED)
         self.done = False
+        self.i_move = 0
         return (self.board, self.pos)
 
     def get_next_state(self, a):
-        self.old_pos = self.pos
+        self.old_pos = self.pos 
+        self.i_move += 1
         if a==0: # left
             self.pos = max(self.old_pos-1, 0)
         elif a==1: # stay 
@@ -22,6 +27,8 @@ class JetsonEnv():
                 self.done = True
         elif a==2: # right
             self.pos = min(self.old_pos+1, self.nLED-1)
+        if self.i_move >= self.max_move: # check if max moves
+            self.done = True
             
     def get_reward(self, a):
         rew = -1
